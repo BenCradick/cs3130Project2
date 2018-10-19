7588f6ded8c139fbbcaa30f4c99bc46e5dbde0c0
Ben Cradick <ben.cradick@gmail.com>
2018-10-19 18:05:30 -0500

    sorting algorithms implemented

--- CRAB2A.py
+++ CRAB2A.py
@@ -1,13 +1,154 @@
-def selection_sort():
+#Ben Cradick
+#cs3130
+#10-19-18
+def selection_sort(arr):
+    for i in range(len(arr)): 
+        # Find the minimum element in remaining  
+        # unsorted array 
+        min_idx = i 
+        for j in range(i+1, len(arr)): 
+            if arr[min_idx] > arr[j]: 
+                min_idx = j 
+                
+        # Swap the found minimum element with  
+        # the first element         
+        arr[i], arr[min_idx] = arr[min_idx], arr[i] 
 
-def insertion_sort():
+def insertion_sort(arr):
+    
+  
+    # Traverse through 1 to len(arr) 
+    for i in range(1, len(arr)): 
+  
+        key = arr[i] 
+  
+        # Move elements of arr[0..i-1], that are 
+        # greater than key, to one position ahead 
+        # of their current position 
+        j = i-1
+        while j >=0 and key < arr[j] : 
+                arr[j+1] = arr[j] 
+                j -= 1
+        arr[j+1] = key 
 
-def bubble_sort_counting():
+def bubble_sort_counting(arr):
+    n = len(arr) 
+   
+    # Traverse through all array elements 
+    for i in range(n): 
+        swapped = False
+  
+        # Last i elements are already 
+        #  in place 
+        for j in range(0, n-i-1): 
+   
+            # traverse the array from 0 to 
+            # n-i-1. Swap if the element  
+            # found is greater than the 
+            # next element 
+            if arr[j] > arr[j+1] : 
+                arr[j], arr[j+1] = arr[j+1], arr[j] 
+                swapped = True
+  
+        # IF no two elements were swapped 
+        # by inner loop, then break 
+        if swapped == False: 
+            break
 
-def bubble_sort_no_counting():
 
-def quick_sort():
+def bubble_sort_no_counting(arr):
+    n = len(arr) 
+  
+    # Traverse through all array elements 
+    for i in range(n): 
+  
+        # Last i elements are already in place 
+        for j in range(0, n-i-1): 
+  
+            # traverse the array from 0 to n-i-1 
+            # Swap if the element found is greater 
+            # than the next element 
+            if arr[j] > arr[j+1] : 
+                arr[j], arr[j+1] = arr[j+1], arr[j] 
 
-def merges_sort():
+def quick_sort(arr, low, high):
+    if low < high: 
+  
+        # pi is partitioning index, arr[p] is now 
+        # at right place 
+        pi = partition(arr,low,high) 
+  
+        # Separately sort elements before 
+        # partition and after partition 
+        quick_sort(arr, pi+1, high) 
+        quick_sort(arr, low, pi-1) 
 
-def select_pivot():
+def merge_sort(arr, l, r):
+    if l < r: 
+  
+        # Same as (l+r)/2, but avoids overflow for 
+        # large l and h 
+        m = (l+(r-1))/2
+  
+        # Sort first and second halves 
+        merge_sort(arr, l, m) 
+        merge_sort(arr, m+1, r) 
+        merge(arr, l, m, r) 
+
+def partition(arr, low, high):
+    i = ( low-1 )         # index of smaller element 
+    pivot = arr[high]     # pivot 
+  
+    for j in range(low , high): 
+        # If current element is smaller than or 
+        # equal to pivot 
+        if   arr[j] <= pivot: 
+          
+            # increment index of smaller element 
+            i = i+1 
+            arr[i],arr[j] = arr[j],arr[i] 
+  
+    arr[i+1],arr[high] = arr[high],arr[i+1] 
+    return ( i+1 ) 
+def merge(arr, l, m, r): 
+    n1 = m - l + 1
+    n2 = r- m 
+  
+    # create temp arrays 
+    L = [0] * (n1) 
+    R = [0] * (n2) 
+  
+    # Copy data to temp arrays L[] and R[] 
+    for i in range(0 , n1): 
+        L[i] = arr[l + i] 
+  
+    for j in range(0 , n2): 
+        R[j] = arr[m + 1 + j] 
+  
+    # Merge the temp arrays back into arr[l..r] 
+    i = 0     # Initial index of first subarray 
+    j = 0     # Initial index of second subarray 
+    k = l     # Initial index of merged subarray 
+  
+    while i < n1 and j < n2 : 
+        if L[i] <= R[j]: 
+            arr[k] = L[i] 
+            i += 1
+        else: 
+            arr[k] = R[j] 
+            j += 1
+        k += 1
+  
+    # Copy the remaining elements of L[], if there 
+    # are any 
+    while i < n1: 
+        arr[k] = L[i] 
+        i += 1
+        k += 1
+  
+    # Copy the remaining elements of R[], if there 
+    # are any 
+    while j < n2: 
+        arr[k] = R[j] 
+        j += 1
+        k += 1
